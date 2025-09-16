63d12f14902e6e36d667c148c861c196d57096cd
Alex Devine <adevine@wris.com>
2025-09-16 17:02:15 -0400

    Added 'SET' to autocommit statements and semicolons

---
+++ createViews.sql
@@ -0,0 +1,12 @@
+/* 
+
+For View_User_Information
+- Join users, current_city, hometown_city, educations (all on user_id)
+
+For View_Are_Friends
+- 
+
+For View_Photo_Information
+- 
+
+*/
--- loadData.sql
+++ loadData.sql
@@ -75,7 +75,7 @@ SELECT DISTINCT u1, u2
     ) AS pairs
 WHERE u1 < u2
 
-AUTOCOMMIT OFF
+SET AUTOCOMMIT OFF;
 -- Insert albums
 INSERT INTO Albums (album_id, album_owner_id, album_name, album_created_time, album_modified_time, album_link, album_visibility, cover_photo_id)
 SELECT DISTINCT album_id, owner_id, album_name, album_created_time, album_modified_time, album_link, album_visibility, cover_photo_id
@@ -86,8 +86,8 @@ INSERT INTO Photos (photo_id, album_id, photo_caption, photo_created_time, photo
 SELECT DISTINCT photo_id, album_id, photo_caption, photo_created_time, photo_modified_time, photo_link
 FROM project1.Public_Photo_Information;
 
-COMMIT
-AUTOCOMMIT ON
+COMMIT;
+SET AUTOCOMMIT ON;
 -- Insert tags
 INSERT INTO Tags (tag_photo_id, tag_subject_id, tag_created_time, tag_x, tag_y)
 SELECT DISTINCT photo_id, tag_subject_id, tag_created_time, tag_x_coordinate, tag_y_coordinate
